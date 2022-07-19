7991a7853af9077fd7cdf04b2780e919d747d4cc
nsndhx <18856929728@163.com>
2022-07-19 22:20:56 +0800

    mapreduce

---
+++ aoue_analyzer/src/main/java/com/nd/aoue/analyzer/reducer/StCandReducer.java
@@ -0,0 +1,21 @@
+package com.nd.aoue.analyzer.reducer;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+public class StCandReducer extends Reducer<Text, Text, Text, Text> {
+    @Override
+    protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        //统计各党派支持
+        int StCandAmount=0;
+        for (Text value : values) {
+            int amount=(int)Float.parseFloat(value.toString());
+            StCandAmount+=amount;
+        }
+        System.out.println(key + "_" + StCandAmount);
+
+        context.write(key,new Text(key + "_" + StCandAmount));
+    }
+}
